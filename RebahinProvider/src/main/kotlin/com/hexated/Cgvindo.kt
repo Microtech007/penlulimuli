21119c42f902b937a6d4df3e8e70145c01c8fb29
Microtech007 <140102618+Microtech007@users.noreply.github.com>
2023-12-06 02:41:50 +0800

    xxxxxxx

---
+++ RebahinProvider/src/main/kotlin/com/hexated/Cgvindo.kt
@@ -0,0 +1,9 @@
+package com.hexated
+
+import com.lagradost.cloudstream3.TvType
+
+class Cgvindo : RebahinProvider() {
+    override var mainUrl = "http://167.86.79.231"
+    override var name = "Cgvindo"
+
+}
